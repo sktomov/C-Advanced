05bb641fa99f80d5abe2aa8199c9c82ac5b48389
sktomov <sktomov@gmail.com>
2017-06-04 23:27:28 +0300

    Part II

---
+++ BashSoft/BashSoft/ExceptionMessages.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BashSoft
+{
+	public static class ExceptionMessages
+	{
+		public const string ExampleExceptionMessage = "Example message!";
+		public const string DataAlreadyInitializedException = "Data is already initialized!";
+		
+	}
+}
---
+++ BashSoft/BashSoft/StudentsRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BashSoft
+{
+	public static class StudentsRepository
+	{
+		public static bool isDataInitialized = false;
+		private static Dictionary<string, Dictionary<string, List<int>>> studentsByCourse;
+
+		public static void InitializeData()
+		{
+			if (!isDataInitialized)
+			{
+				OutputWriter.WriteMessageOnNewLine("Reading data...");
+				studentsByCourse = new Dictionary<string, Dictionary<string, List<int>>>();
+				ReadData();
+			}
+			else
+			{
+				OutputWriter.WriteMessageOnNewLine(ExceptionMessages.DataAlreadyInitializedException);
+			}
+		}
+
+		private static void ReadData()
+		{
+			string input = Console.ReadLine();
+			while (!string.IsNullOrEmpty(input))
+			{
+				string[] tokens = input.Split(' ');
+				string course = tokens[0];
+				string student = tokens[1];
+				int mark = int.Parse(tokens[2]);
+				if (!studentsByCourse.ContainsKey(course))
+				{
+					studentsByCourse[course] = new Dictionary<string, List<int>>();
+				}
+				if (!studentsByCourse[course].ContainsKey(student))
+				{
+					studentsByCourse[course][student] = new List<int>();
+				}
+				studentsByCourse[course][student].Add(mark);
+				input = Console.ReadLine();
+			}
+			isDataInitialized = true;
+			OutputWriter.WriteMessageOnNewLine("Data read!");
+		}
+	}
+}
