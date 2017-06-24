624734fd6e5cbb79117c8c76aebdbd145b202a76
sktomov <sktomov@gmail.com>
2017-06-24 12:23:53 +0300

    Finished

--- BashSoft/BashSoft/Repository/RepositoryFilters.cs
+++ BashSoft/BashSoft/Repository/RepositoryFilters.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace BashSoft
 {
@@ -9,15 +10,15 @@ namespace BashSoft
 		{
 			if (wantedFilter == "excellent")
 			{
-				FilterAndTake(wantedData,ExcellentFilter, studentsToTake);
+				FilterAndTake(wantedData,x => x >= 5, studentsToTake);
 			}
 			else if (wantedFilter == "average")
 			{
-				FilterAndTake(wantedData, AverageFilter, studentsToTake);
+				FilterAndTake(wantedData, x => x < 5 && x >= 3.5, studentsToTake);
 			}
 			else if (wantedFilter == "excellent")
 			{
-				FilterAndTake(wantedData, PoorFilter, studentsToTake);
+				FilterAndTake(wantedData, x => x < 3.5, studentsToTake);
 			}
 			else
 			{
@@ -35,8 +36,10 @@ namespace BashSoft
 				{
 					break;
 				}
-				double averageMark = Average(userName_Points.Value);
-				if (givenFilter(averageMark))
+				double averageScore = userName_Points.Value.Average();
+				double percentageOfFullfillments = averageScore / 100;
+				double mark = percentageOfFullfillments * 4 + 2;
+				if (givenFilter(mark))
 				{
 					OutputWriter.PrintStudent(userName_Points);
 					counterForPrinted++;
@@ -44,29 +47,5 @@ namespace BashSoft
 			}
 		}
 
-		private static bool ExcellentFilter(double mark)
-		{
-			return mark >= 5.0;
-		}
-		private static bool AverageFilter(double mark)
-		{
-			return mark < 5.0 && mark >= 3.5;
-		}
-		private static bool PoorFilter(double mark)
-		{
-			return mark < 3.5;
-		}
-
-		private static double Average(List<int> scoresOnTasks)
-		{
-			int totalScore = 0;
-			foreach (var score in scoresOnTasks)
-			{
-				totalScore += score;
-			}
-			var percentageOfAll = totalScore / (scoresOnTasks.Count * 100);
-			var mark = percentageOfAll * 4 + 2;
-			return mark;
-		}
 	}
 }
